778787514f5d65289db67168f2e216a9ceaf0380
Sholana1 <shodunkedavid16@gmail.com>
2024-07-09 08:40:50 +0100

    Create the header section

--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -1,8 +1,21 @@
+const flowbite = require("flowbite-react/tailwind");
+
 /** @type {import('tailwindcss').Config} */
-export default {
-  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
-  theme: {
-    extend: {},
-  },
-  plugins: [],
+module.exports = {
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}", flowbite.content()],
+  plugins: [
+    // ...
+    flowbite.plugin(),
+  ],
 };
+
+// const flowbite = require("flowbite-react/tailwind");
+
+// /** @type {import('tailwindcss').Config} */
+// export default {
+//   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}", flowbite.content()],
+//   theme: {
+//     extend: {},
+//   },
+//   plugins: [flowbite.plugin()],
+// };
